e88b9e37606d6e5304cc7ecc5b5c4dbc2fa7ab76
paulXLV <paul@excelvertical.com>
2018-08-24 06:14:31 -0400

    HW almost there.. Finally got Average Over Period.. Now just have to format output and clean code with comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,7 @@ import csv
 total1=[] # check to see if total sum is correct
 profit_loss=[]
 pl_summary={}
+mom=[]
 with open('\\Users\\Paul-DS\\Downloads\\budget_data.csv', newline='') as csvfile:
     # read and split the data on commas and put into string variable budget_reader
     budget_reader=csv.reader(csvfile,delimiter=',')
@@ -25,27 +26,45 @@ with open('\\Users\\Paul-DS\\Downloads\\budget_data.csv', newline='') as csvfile
         total1.append(int(line[1]))
         total+=int(line[1])
         total_months+=1
+       
         
     # find Largest increase/decrease and save month-year and revenue together in list
     # First initialize lowest and highest lists with next value on list
     min_value=profit_loss[1][1]
     max_value=profit_loss[1][1]
+    
     for value in profit_loss:
-        # print(value)
         if int(value[1]) < int(min_value):
             min_month_yr=value[0]
             min_value=value[1]
-           # print(min_value)
         elif int(value[1]) > int(max_value):
             max_value=value[1]
             max_month_yr=value[0]
-           # print(max_value)
-    
-
+           
+    subtract_MoM=0
+    tot_MoM=0
+    Avg_Mom=0
+    max_decrease=int(profit_loss[total_months-1][1])-int(profit_loss[total_months-2][1])
+    max_increase=int(profit_loss[total_months-1][1])-int(profit_loss[total_months-2][1])
+    for i in range(total_months,1,-1):
+        subtract_MoM=int(profit_loss[i-1][1])-int(profit_loss[i-2][1])
+        if subtract_MoM < max_decrease:
+            min_month_yr=profit_loss[i-1][0]
+            max_decrease=subtract_MoM
+        elif subtract_MoM > max_increase:
+            max_increase=subtract_MoM
+            max_month_yr=profit_loss[i-1][0]
+        tot_MoM=tot_MoM+subtract_MoM
+        
+    Avg_MoM=tot_MoM/(total_months-1)
+        
+        
+        
 # print(profit_loss)
 print('month count: ', total_months)
 print('total: ',total)
-print('highest: ',max_month_yr,', ',max_value)
-print('lowest: ',min_month_yr,', ',min_value)
+print('highest: ',max_month_yr,', ',max_value,', ', max_increase)
+print('lowest: ',min_month_yr,', ',min_value,', ', max_decrease)
+print('Month Over Month Average: ', round(Avg_MoM,2))
 
 #print(avg(total1))
